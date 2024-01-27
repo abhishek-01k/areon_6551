32e251219330efcec7c19077fca752364849cce4
kamalbuilds <geniusamansingh@gmail.com>
2024-01-28 02:41:38 +0530

    arnft

--- tbaareoncontracts/contracts/ArNFT.sol
+++ tbaareoncontracts/contracts/ArNFT.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract myNFT is ERC721, Ownable {
-    constructor() ERC721("myNFT", "NFT") {}
+contract ArNFT is ERC721, Ownable {
+    constructor() ERC721("ArNFT", "NFT") {}
 
     function safeMint(address to, uint256 tokenId) public onlyOwner {
         _safeMint(to, tokenId);
